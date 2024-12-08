5bf334c9c2e3bc0a244fa906ebdb2b7e7c207a82
Leka Uïla <liam.flandrinck.58@gmail.com>
2024-12-09 00:05:11 +0100

    remove useless line in parsing

--- srcs/parsing.cpp
+++ srcs/parsing.cpp
@@ -6,7 +6,7 @@
 /*   By: lflandri <liam.flandrinck.58@gmail.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/08 20:49:13 by lflandri          #+#    #+#             */
-/*   Updated: 2024/12/08 23:23:10 by lflandri         ###   ########.fr       */
+/*   Updated: 2024/12/09 00:00:57 by lflandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,24 +48,20 @@ std::vector<Vec3> parse(char *name)
 			std::string new_line;
 			std::getline(file, new_line);
 			std::vector<std::string> string_list = split(new_line, ' ');
-			// std::cout << string_list.size() << std::endl;
-			if (string_list.size() == 0)
+			if (string_list.size() == 0)//empty line check
 				continue;
-			if (string_list[0][0] == '#')
+			if (string_list[0][0] == '#')//comment line
 				continue;
-			if (string_list.size() != 3)
+			if (string_list.size() != 3)// check number of coordonate for point
 				throw std::invalid_argument("Error : invalid number of argument for a point.");
 			for (std::string str : string_list)
 			{
-				// std::cout << "test1 " << str.size() << std::endl;
 				for ( int i = 0; i < str.size(); i++)
 				{
-					// std::cout << "test2" << std::endl;
 					if (str[i] != '0')
 					{
 						if (!isdigit(str[i]))
 							throw std::invalid_argument("Error : point cordonate need to pe represented as digit.");
-						// std::cout << str.size() << " | " << i << " | " << str.size() - i << std::endl;
 						if (str.size() - i > 4)
 							throw std::invalid_argument("Error : Number for point coordonate can't be upper than 9999.");
 					}
@@ -73,9 +69,6 @@ std::vector<Vec3> parse(char *name)
 				}
 
 			}
-			// std::cout << atoi(string_list[0].c_str()) << std::endl;
-			// std::cout << atoi(string_list[1].c_str()) << std::endl;
-			// std::cout << atoi(string_list[2].c_str()) << std::endl;
 			Vec3 vec = Vec3(atoi(string_list[0].c_str()), atoi(string_list[1].c_str()), atoi(string_list[2].c_str()));
 			point_list.push_back(vec);
 		}

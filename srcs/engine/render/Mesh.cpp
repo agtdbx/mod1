2e75bc766c9227719d9c7f2f55f761b473f04fb6
Gugus <gugus.deroubaix@gmail.com>
2024-12-10 18:14:29 +0100

    Add transformantion

--- srcs/engine/render/Mesh.cpp
+++ srcs/engine/render/Mesh.cpp
@@ -176,6 +176,13 @@ void	Mesh::drawWithTexture(Shader *shader, TextureManager *textureManager, std::
 
 	shader->use();
 
+	glm::mat4 trans = glm::mat4(1.0f);
+	trans = glm::translate(trans, glm::vec3(0.0f, 0.0f, 0.0f));
+	trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(1.0f, 1.0f, 0.0f));
+
+	unsigned int transformLoc = glGetUniformLocation(shader->getShaderId(), "transform");
+	glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
+
 	glBindTexture(GL_TEXTURE_2D, texture);
 	glBindVertexArray(shader->getVAOId());
 	glDrawElements(GL_TRIANGLES, this->nbIndices, GL_UNSIGNED_INT, 0);
